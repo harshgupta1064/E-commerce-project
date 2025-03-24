99da02d3461433959fa03b4cbe853608bc539ff3
harshgupta1064 <harshgupta1064@gmail.com>
2025-03-24 20:29:19 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:t_store/app.dart';
 
 void main() {
+  // Todo : Add Widgets Bindings
+  // Todo : Init Local Storage
+  // Todo : Await Local Storage
+  // Todo : Init Firebase
+  // Todo : Initialise Authentication
   runApp(const App());
 }
